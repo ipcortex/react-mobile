f295f716aa068d275a664d17520b894587f51638
Ben Gesoff <ben.gesoff@gmail.com>
2018-04-03 17:26:17 +0100

    Add icons and make look nice

--- app/components/RecentCall.js
+++ app/components/RecentCall.js
@@ -6,21 +6,38 @@ import {
   View,
   Text
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import moment from 'moment';
+
+const phoneIcons = {
+	'caller': 'call-made',
+	'callee': 'call-received',
+	'missed': 'call-missed',
+	'noanswer': 'call-made'
+};
 
 export default function RecentCall({call}) {
 	return (
 		<View style={{
 			flexDirection: 'row',
 			justifyContent: 'space-between',
-			alignItems: 'center',
+			alignItems: 'flex-start',
 			paddingHorizontal: 25,
 			paddingVertical: 3
 		}}>
-			<View>
-				<Text style={{fontSize: 20}}>{call.remoteName}</Text>
-				<Text style={{color: '#bbb'}}>{call.remoteNumber}</Text>
+		<View style={{flexDirection: 'row'}}>
+				<Icon 
+					name={phoneIcons[call.party]}
+					style={{
+						color: '#ddd'
+					}}
+					size={22}
+				/>
+				<View style={{paddingHorizontal: 25}}>
+					<Text style={{fontSize: 20}}>{call.remoteName}</Text>
+					<Text style={{color: '#bbb'}}>{call.remoteNumber}</Text>
+				</View>
 			</View>
 			<Text>{formatDate(call.stamp)}</Text>
 		</View>
